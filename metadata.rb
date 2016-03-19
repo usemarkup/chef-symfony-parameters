495ef32eb889dbf9cff783f7a08d5033353f5c4a
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-03-19 13:11:30 +0000

    chore(version) - bumps for permissions change

--- metadata.rb
+++ metadata.rb
@@ -2,4 +2,4 @@ name             'symfony-parameters'
 license          'All rights reserved'
 description      'Symfony Parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
