d0ec966210dc6ede780c01eee2ee86034a0f95a8
Gavin Staniforth <gavin@usemarkup.com>
2019-10-29 23:29:34 +0000

    chore: release 3.0.1

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'symfony-parameters'
 license 'MIT Licence'
 description      'Symfony Parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.0.1'
 
 maintainer 'Gavin Staniforth'
 maintainer_email 'gavin@usemarkup.com'
